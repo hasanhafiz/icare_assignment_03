845b3bdb91d85f8fedef5f942d6c56ed3be80aa9
hasanhafiz <hasanhafizsakil@gmail.com>
2024-07-14 01:14:11 +0600

    Add user validation

--- classes/Validate.php
+++ classes/Validate.php
@@ -2,20 +2,47 @@
 class Validate {
     private $_passed = false;
     private $_erros = [];
-    private $_db = null;
     
     // initialize db
-    public function __construct( $filename = 'users' )
+    public function __construct()
     {
-        // $this->_db = new JsonFileManager( $filename );
     }
     
-    public function check( $source, $items = [] ) {
+    public function check( $source, $items = [] ) {                
+        // check required field
+        if ( !empty( $items ) ) {
+            foreach ($items as $key => $item) {                
+                if ( ! Input::get($item)  ) {
+                    $this->addError("{$item} is required!");
+                }
+            }
+        }              
+        
+        // check user already exists 
+        if ( !empty( $source['email'] ) ) {            
+            $user = new User();
+            $user_email = $source['email'];
+            if ( $user->exists( $user_email ) ) {
+                $this->addError("{$user_email} is already exists!");
+            }                                  
+        }
+        
+        // check password mismatch
+        if ( !empty( $source['password'] ) && !empty( $source['confirm_password'] ) ) {                                
+            if ( $source['password'] == $source['confirm_password']) {
+                $this->addError("Password does not match!");
+            }                 
+        }
+        
+        if ( empty( $this->_erros ) ) {
+            $this->_passed = true;
+        }
+                
+        return $this;        
     }
     
     public function passed() {
-        return true;
-        // return $this->_passed;
+        return $this->_passed;
     }
     
     public function addError(string $error): void
